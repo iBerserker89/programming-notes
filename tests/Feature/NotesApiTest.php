fdbe2af40ed76db0df9b2a62b559b2935106be46
Luciano Barros <lucianof.barros89@gmail.com>
2026-09-21 20:51:39 -0300

    test: document update note not found test

--- tests/Feature/NotesApiTest.php
+++ tests/Feature/NotesApiTest.php
@@ -173,6 +173,15 @@ class NotesApiTest extends TestCase
         $response->assertNotFound();
     }
 
+    /**
+     * Verifies the behavior when updating a note that does not exist.
+     *
+     * Since RefreshDatabase gives this test a clean database and
+     * no note with this ID is created, HTTP 404 is expected.
+     *
+     * This behavior is provided by findOrFail()
+     * inside the NoteController.
+     */
     public function test_update_note_not_found(): void
     {
         $response = $this->patchJson('/notes/999999', [
